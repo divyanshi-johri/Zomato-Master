ab7153f2c3c48774ef9a3ce9f820d692ed4ca784
Divuu <86949530+divyanshiJohari@users.noreply.github.com>
2021-10-21 13:21:55 +0530

    Delete Client__ directory

--- Client__/src/Redux/Reducer/root.Reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-import restaurant from "./restaurant/restaurant.reducer";
-
-const rootReducer = combineReducers({restaurant});
-
-export default rootReducer;           
